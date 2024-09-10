02e8285334f8c6a489e613b5490394d7abd16d19
Hari Prasad Sanjel <121128703+hpsanjel@users.noreply.github.com>
2024-09-10 21:23:45 +0200

    Updated css styles for delivery boy

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
-'use strict';
-
-
+"use strict";
 
 /**
  * navbar toggle
@@ -11,18 +9,16 @@ const navbarLinks = document.querySelectorAll("[data-nav-link]");
 const menuToggleBtn = document.querySelector("[data-menu-toggle-btn]");
 
 menuToggleBtn.addEventListener("click", function () {
-  navbar.classList.toggle("active");
-  this.classList.toggle("active");
+	navbar.classList.toggle("active");
+	this.classList.toggle("active");
 });
 
 for (let i = 0; i < navbarLinks.length; i++) {
-  navbarLinks[i].addEventListener("click", function () {
-    navbar.classList.toggle("active");
-    menuToggleBtn.classList.toggle("active");
-  });
+	navbarLinks[i].addEventListener("click", function () {
+		navbar.classList.toggle("active");
+		menuToggleBtn.classList.toggle("active");
+	});
 }
-
-
 
 /**
  * header sticky & back to top
@@ -32,36 +28,14 @@ const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 window.addEventListener("scroll", function () {
-  if (window.scrollY >= 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
-  }
+	if (window.scrollY >= 100) {
+		header.classList.add("active");
+		backTopBtn.classList.add("active");
+	} else {
+		header.classList.remove("active");
+		backTopBtn.classList.remove("active");
+	}
 });
-
-
-
-/**
- * search box toggle
- */
-
-const searchBtn = document.querySelector("[data-search-btn]");
-const searchContainer = document.querySelector("[data-search-container]");
-const searchSubmitBtn = document.querySelector("[data-search-submit-btn]");
-const searchCloseBtn = document.querySelector("[data-search-close-btn]");
-
-const searchBoxElems = [searchBtn, searchSubmitBtn, searchCloseBtn];
-
-for (let i = 0; i < searchBoxElems.length; i++) {
-  searchBoxElems[i].addEventListener("click", function () {
-    searchContainer.classList.toggle("active");
-    document.body.classList.toggle("active");
-  });
-}
-
-
 
 /**
  * move cycle on scroll
@@ -69,24 +43,22 @@ for (let i = 0; i < searchBoxElems.length; i++) {
 
 const deliveryBoy = document.querySelector("[data-delivery-boy]");
 
-let deliveryBoyMove = -80;
+let deliveryBoyMove = 50;
 let lastScrollPos = 0;
 
 window.addEventListener("scroll", function () {
+	let deliveryBoyTopPos = deliveryBoy.getBoundingClientRect().top;
 
-  let deliveryBoyTopPos = deliveryBoy.getBoundingClientRect().top;
+	if (deliveryBoyTopPos < 400 && deliveryBoyTopPos > -500) {
+		let activeScrollPos = window.scrollY;
 
-  if (deliveryBoyTopPos < 500 && deliveryBoyTopPos > -250) {
-    let activeScrollPos = window.scrollY;
+		if (lastScrollPos < activeScrollPos) {
+			deliveryBoyMove += 1;
+		} else {
+			deliveryBoyMove -= 1;
+		}
 
-    if (lastScrollPos < activeScrollPos) {
-      deliveryBoyMove += 1;
-    } else {
-      deliveryBoyMove -= 1;
-    }
-
-    lastScrollPos = activeScrollPos;
-    deliveryBoy.style.transform = `translateX(${deliveryBoyMove}px)`;
-  }
-
+		lastScrollPos = activeScrollPos;
+		deliveryBoy.style.transform = `translateX(${deliveryBoyMove}px)`;
+	}
 });
